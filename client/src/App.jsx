e25e51da4b088bb20fa1316186918eeb6582a272
juam-sv <juam13pb@gmail.com>
2022-01-16 21:42:54 -0300

    Adiçao do logo, finalizaçao da navbar e conclusao da parte inicial da tela inicial/welcome

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,6 @@
 import { Navbar, Welcome, Footer, Services, Transactions } from "./components"
 
 const App = () => {
-  
   return (
     <div className="min-h-screen">
       <div className="gradient-bg-welcome">
