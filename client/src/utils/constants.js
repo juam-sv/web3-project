63989cca2e180196494f8c2c62e809ab184f371c
Juam Sousa <juam13pb@gmail.com>
2022-01-25 21:35:59 -0300

    Finalização da preparação do contrato e configuração do deploy

---
+++ client/src/utils/constants.js
@@ -0,0 +1,5 @@
+import abi from './Transactions.json';
+
+export const contractABI = abi.abi;
+
+export const contractAddress = '0xf2aCa3099b6Cfce155445c50C669f155EDFb4Beb';
--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,21 +1,14 @@
-require("@nomiclabs/hardhat-waffle");
+// 
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+require('@nomiclabs/hardhat-waffle');
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
-  solidity: "0.8.4",
-};
+  solidity: '0.8.0',
+  networks: {
+    ropsten: {
+      url: 'https://eth-ropsten.alchemyapi.io/v2/kI1WwxNUXn-4B7atlzbQVenQr0N6bMpj',
+      accounts: [ '07d4a85a3357d15ba06890897951e4e6bb8c37cafd1dad3a5f54997a488c644d' ]
+    }
+  }
+}
+
